c127a0f2b5200e95b920dc8afa60aed6f3f1a265
kornaa56 <a.a.kornoukhova.3652@gmail.com>
2025-05-23 20:58:42 +0300

    Merge remote changes into main branch

--- parser.py
+++ parser.py
@@ -20,12 +20,12 @@ def create_parser():
     parser.add_argument("-f","--filename", type=str, required=True, help="Input file with sequences to align")
     parser.add_argument("-a","--alignment_mode", type=str, choices=("unaligned", "aligned"), required=True, 
                         help='Choose the format of sequences: {unaligned, aligned}')
-    parser.add_argument("--gap-open", type=float, required=True, help="Penalty for gap opening")
-    parser.add_argument("--gap-extension", type=float, required=True, help="Penalty for gap extension")
+    parser.add_argument("--gap-open", type=float, default = 1, help="Penalty for gap opening")
+    parser.add_argument("--gap-extension", type=float, default= 0.5, help="Penalty for gap extension")
     parser.add_argument("-m","--molecule", type=str, choices=('DNA', 'protein'), required=True, 
                         help='Choose the type of sequences: {DNA, protein}')
     parser.add_argument("--match", type=float,  default = 5,  help="Bonus for match")
-    parser.add_argument("--mismatch", type=float, default=-4, help="Penalty for mismatch")
+    parser.add_argument("--mismatch", type=float, default= 4, help="Penalty for mismatch")
     return parser
 
 
